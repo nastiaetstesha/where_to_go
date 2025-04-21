64f02a35c2577ce5ad858403ede8ac2b2adfd23e
Avakova Anastasiia <avakova.ast@gmail.com>
2025-04-21 12:33:57 +0300

    add photo preview in admin

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Place, PlaceImage
 
 
 class PlaceImageInline(admin.TabularInline):
     model = PlaceImage
     extra = 1
+    readonly_fields = ['preview']
+    fields = ['image', 'preview', 'order']
+
+    def preview(self, obj):
+        if obj.image:
+            return format_html(
+                '<img src="{}" style="max-height: 200px;" />',
+                obj.image.url
+                )
+        return "Нет изображения"
+    preview.short_description = "Превью"
 
 
 @admin.register(Place)
